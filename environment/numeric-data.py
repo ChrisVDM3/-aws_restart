de6102e4b836a5982848727f32bd5f18c7237c84
ChrisVDM3 <chrisgamboaupa2017@gmail.com>
2024-08-13 09:53:56 -0600

    Add files via upload

---
+++ environment/numeric-data.py
@@ -0,0 +1,25 @@
+print("Python has three numeric types: int, float, and complex")
+
+Myvalue=1
+print (type(Myvalue))
+print(str(Myvalue) + " is of the data type " + str(type(Myvalue)))
+
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
